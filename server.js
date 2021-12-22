1d83afdf5b80cf189fb0474b1ecb6e280763647a
Fabian Felix Selbach <ffs2309.fs@gmail.com>
2021-12-22 09:53:32 +0100

    Keep file contents cached and only access file on program start/end

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ const app = express();
 
 const filename = 'imageUrls.json';
 
+var fileContent = JSON.parse("[]");
+
 app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send(fs.readFileSync(filename));
+    res.send(JSON.stringify(fileContent));
 });
 
 app.post('/set', (req, res) => {
-    fs.writeFileSync(filename, JSON.stringify(req.body))
+    fileContent = req.body;
     res.send('ok');
 })
 
@@ -22,5 +24,13 @@ let server = app.listen(8081, () => {
     const host = 'localhost'
     const port = server.address().port
 
+    fileContent = JSON.parse(fs.readFileSync(filename).toString())
     console.log("Example app listening at http://%s:%s", host, port)
 })
+
+process.on("SIGTERM", endProgram).on("SIGINT", endProgram)
+
+function endProgram(){
+    fs.writeFileSync(filename, JSON.stringify(fileContent))
+    server.close()
+}
